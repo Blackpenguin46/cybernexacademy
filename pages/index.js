0da0d7ca688af5e11c67b766a273b3131b425a86
Sam oakes <samoakescyber@proton.me>
2025-02-26 19:15:03 -0500

    update

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>CyberNex</h1>
+      <p>Your site is being deployed...</p>
+    </div>
+  );
+} 
