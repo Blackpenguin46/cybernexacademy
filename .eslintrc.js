eae85f87c04c393e5122788e6513be8e7a72d3cd
Sam oakes <samoakescyber@proton.me>
2025-02-28 20:47:44 -0500

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,7 @@
 module.exports = {
   root: true,
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/no-unescaped-entities": "off",
-    "react/display-name": "off",
-    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-    "import/no-anonymous-default-export": "off"
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  ignorePatterns: [".next", "node_modules", "out"]
+  // This will ignore all files
+  ignorePatterns: ['**/*'],
+  extends: [],
+  rules: {}
 }; 
