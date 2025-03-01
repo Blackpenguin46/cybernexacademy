3f0fba89742b567d2ed55fd4a8c3ffd71ba74fcd
Sam oakes <samoakescyber@proton.me>
2025-02-28 19:23:14 -0500

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
+  root: true,
   extends: ["next/core-web-vitals"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "react/no-unescaped-entities": "off",
     "react/display-name": "off",
-    "no-unused-vars": "warn",
+    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
     "import/no-anonymous-default-export": "off"
   },
   parserOptions: {
@@ -14,5 +15,6 @@ module.exports = {
     ecmaFeatures: {
       jsx: true
     }
-  }
+  },
+  ignorePatterns: [".next", "node_modules", "out"]
 }; 
