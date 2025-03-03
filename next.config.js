4cf80ec1d99b27a69bee4b7c29835983d8da19cf
Sam oakes <samoakescyber@proton.me>
2025-03-03 18:04:47 -0500

    Fix 404 error, add not-found page and proper routing configurations

--- next.config.js
+++ next.config.js
@@ -8,6 +8,34 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/learning-resources',
+        destination: '/',
+      },
+      {
+        source: '/community',
+        destination: '/',
+      },
+      {
+        source: '/careers',
+        destination: '/',
+      },
+      {
+        source: '/college-students',
+        destination: '/',
+      },
+      {
+        source: '/tools-utilities',
+        destination: '/',
+      },
+      {
+        source: '/cybernex-plus',
+        destination: '/',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
