c0102c4961b34192b44ef58c512c36bb79792b53
Sam oakes <samoakescyber@proton.me>
2025-03-03 12:42:35 -0500

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import Layout from '../components/Layout'
+import { Card } from '../components/ui/card'
+import Button from '../components/ui/button'
 import Link from 'next/link'
 
 export default function Home() {
