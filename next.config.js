1b611b1942c0b346d8ff757aa4c3e185b7bd3df3
Sam oakes <samoakescyber@proton.me>
2025-04-04 23:10:00 -0400

    Update Content Security Policy to allow Supabase connections

--- next.config.js
+++ next.config.js
@@ -14,14 +14,15 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval';
-      script-src-elem 'self' 'unsafe-inline' *.vercel.app vercel.app;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vxxpwaloyrtwvpmatzpc.supabase.co vxxpwaloyrtwvpmatzpc.supabase.co;
+      script-src-elem 'self' 'unsafe-inline' *.vercel.app vercel.app *.vxxpwaloyrtwvpmatzpc.supabase.co vxxpwaloyrtwvpmatzpc.supabase.co;
       style-src 'self' 'unsafe-inline' fonts.googleapis.com *.googleapis.com;
       style-src-elem 'self' 'unsafe-inline' fonts.googleapis.com *.googleapis.com;
       img-src 'self' vxxpwaloyrtwvpmatzpc.supabase.co thehackernews.com *.thehackernews.com feeds.feedburner.com *.feedburner.com *.bleepingcomputer.com krebsonsecurity.com *.darkreading.com securityweek.com *.securityweek.com data: blob: *;
       font-src 'self' fonts.gstatic.com *.gstatic.com data:;
-      connect-src 'self' thehackernews.com *.thehackernews.com api.rss2json.com *.rss2json.com api.factmaven.com *.factmaven.com www.thehackernews.com feeds.feedburner.com *.feedburner.com feedburner.com *.bleepingcomputer.com krebsonsecurity.com *.darkreading.com securityweek.com *.securityweek.com;
+      connect-src 'self' *.vxxpwaloyrtwvpmatzpc.supabase.co vxxpwaloyrtwvpmatzpc.supabase.co thehackernews.com *.thehackernews.com api.rss2json.com *.rss2json.com api.factmaven.com *.factmaven.com www.thehackernews.com feeds.feedburner.com *.feedburner.com feedburner.com *.bleepingcomputer.com krebsonsecurity.com *.darkreading.com securityweek.com *.securityweek.com;
       frame-src thehackernews.com *.thehackernews.com *.bleepingcomputer.com krebsonsecurity.com *.darkreading.com securityweek.com *.securityweek.com;
+      worker-src 'self' blob:;
     `.replace(/\s{2,}/g, ' ').trim();
 
     return [
